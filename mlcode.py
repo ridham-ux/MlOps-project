76f143d919409d6304fc67fa5bdbd563c717c7d0
Ridham Dogra <ridham.dogra141@gmail.com>
2020-05-24 21:37:43 +0530

    CNN code using Mnist Dataset

--- mlcode.py
+++ mlcode.py
@@ -1,16 +1,13 @@
 #loading the dataset
 from keras.datasets import mnist
 dataset = mnist.load_data('mymnist.db')
-
 train , test = dataset
 X_train , y_train = train
 X_test , y_test = test
-
 X_train = X_train.reshape(60000,28,28,1)
 X_test = X_test.reshape(10000,28,28,1)
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
-
 from keras.utils.np_utils import to_categorical
 y_train_cat = to_categorical(y_train)
 y_test= to_categorical(y_test)
@@ -30,7 +27,7 @@ for i in range(i):
                           activation='relu',
                           input_shape=(28,28,1)
                            ))
-        n=n*2
+	n=n*2
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Flatten())
 model.add(Dense(units=30, activation='relu'))
