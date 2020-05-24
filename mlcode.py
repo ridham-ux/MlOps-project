3ac006136aaf3199a33d214e9e521980e9c508de
Ridham Dogra <ridham.dogra141@gmail.com>
2020-05-24 21:33:02 +0530

    CNN code using Mnist Dataset

--- mlcode.py
+++ mlcode.py
@@ -1,3 +1,4 @@
+#loading the dataset
 from keras.datasets import mnist
 dataset = mnist.load_data('mymnist.db')
 
@@ -5,18 +6,15 @@ train , test = dataset
 X_train , y_train = train
 X_test , y_test = test
 
-
 X_train = X_train.reshape(60000,28,28,1)
 X_test = X_test.reshape(10000,28,28,1)
 X_train = X_train.astype('float32')
 X_test = X_test.astype('float32')
 
-
 from keras.utils.np_utils import to_categorical
 y_train_cat = to_categorical(y_train)
 y_test= to_categorical(y_test)
-
-
+#Adding layers
 from keras.layers import Convolution2D
 from keras.layers import MaxPooling2D
 from keras.layers import Flatten
@@ -32,22 +30,18 @@ for i in range(i):
                           activation='relu',
                           input_shape=(28,28,1)
                            ))
-n=n*2
+        n=n*2
 model.add(MaxPooling2D(pool_size=(2, 2)))
-
 model.add(Flatten())
 model.add(Dense(units=30, activation='relu'))
-
-
 model.add(Dense(units=10, activation='softmax'))
 model.summary()
 model.compile( optimizer = adam(lr=0.01), loss='categorical_crossentropy', metrics=['accuracy'])
-
 trained_model=model.fit(X_train, y_train_cat, epochs=1, validation_data=(X_test,y_test))
 acc=trained_model.history['accuracy'][-1:][0]
 accuracy=acc*100
 print("Accuracy of the model is =",accuracy)
-
+#saving the model accuracy inside output.txt file
 file=open("/cnndata/output.txt","w")
 file.write(str(int(accuracy)))
 file.close()
