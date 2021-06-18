2b749beb488b566e0ecf433980bf9bc94baf1e24
Ridham Dogra <53050466+ridham-ux@users.noreply.github.com>
2021-06-18 21:54:38 +0530

    Update mail.py

--- mail.py
+++ mail.py
@@ -5,7 +5,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 sender_address="ridham.dogra141@gmail.com"
-sender_pass="radu@2000rr"
+sender_pass="radu@2000r"
 reciever_address="ridham.dogra141@gmail.com"
 subject="Regarding The model's accuray"
 content="Hello there, Your model has ben trained and it has given the accuracy "+str(w)+" percentage"
